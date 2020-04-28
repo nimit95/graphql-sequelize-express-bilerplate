81214b574471eaa04714e45fe44c766b717bbdce
Nimit <nimitagg95@gmail.com>
2020-04-28 20:18:44 +0530

    dummy schema and resolver
    Signed-off-by: Nimit <nimitagg95@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-import http from 'http';
+/* eslint-disable no-console */
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
-http.createServer((req, res) => {
-  res.end('help world');
-}).listen(8989);
+const typesArray = fileLoader(path.join(__dirname, './schema'));
+const typeDefs = mergeTypes(typesArray);
+
+const resolversArray = fileLoader(path.join(__dirname, './resolvers'));
+const resolvers = mergeResolvers(resolversArray);
+
+const app = express();
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.listen(
+  { port: 4000 },
+  () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
